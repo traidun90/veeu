186c5419e21f68e2f8e351f8426db4a1d5cb3d40
traidun09 <traidun90@mail.ru>
2019-01-03 17:09:04 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -80,8 +80,8 @@ if (file_exists($include)) {
 	} 
 else 
     {
-    echo "$red"."[!]"."$putih конфиг не найден\n";
-    sleep(1);
+    //echo "$red"."[!]"."$putih конфиг не найден\n";
+    //sleep(1);
 	$text = 
 '<?php
 $config = [
@@ -106,7 +106,7 @@ ${"GLOBALS"}["lbeuhlfg"]="akun";
 error_reporting(0);
 //echo${${"GLOBALS"}["zuupeplpj"]}."Bot for VEEU\n ".$t;
 //echo$ijo."Bot for VEEU [v.4.0]".$turkis."[final]".$putih."creator: ".$ijo."adidoank".$t;
-$ver = "5.03.212";
+$ver = "5.03.213";
 $release = "BETA";
 echo$ijo."Bot for VEEU [$ver]".$turkis." [$release]".$t.$t;
 //echo${${"GLOBALS"}["mlhvfsqvhbk"]}." Subscribe Youtube Sungging ".$t.$t;
