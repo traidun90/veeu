cd342d362f0b59603f40f0f9e1469ea622e7224b
traidun09 <traidun90@mail.ru>
2019-01-03 17:10:40 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -86,16 +86,13 @@ else
 '<?php
 $config = [
 "",#
-"",#
-"",#
-"",#
 ];
 ?>';
 	$fp = fopen("config.php", "w");
 	fwrite($fp, $text);
 	fclose($fp);
 	
-exit;
+//exit;
     }
 	}
 
@@ -106,7 +103,7 @@ ${"GLOBALS"}["lbeuhlfg"]="akun";
 error_reporting(0);
 //echo${${"GLOBALS"}["zuupeplpj"]}."Bot for VEEU\n ".$t;
 //echo$ijo."Bot for VEEU [v.4.0]".$turkis."[final]".$putih."creator: ".$ijo."adidoank".$t;
-$ver = "5.03.213";
+$ver = "5.03.214";
 $release = "BETA";
 echo$ijo."Bot for VEEU [$ver]".$turkis." [$release]".$t.$t;
 //echo${${"GLOBALS"}["mlhvfsqvhbk"]}." Subscribe Youtube Sungging ".$t.$t;
